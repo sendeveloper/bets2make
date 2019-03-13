e35c3137213fbd544119ba5c49c187a130486a28
Sendeveloper <lightmorning46@gmail.com>
2019-03-13 10:33:34 +0800

    Updated structure

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import configureStore, { history } from './store/configureStore';
-import Root from './components/Root';
+import Root from './pages/Root';
 import './styles/styles.scss';
 // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
 require('./favicon.ico');
@@ -19,9 +19,9 @@ render(
 );
 
 if (module.hot) {
-  module.hot.accept('./components/Root', () => {
+  module.hot.accept('./pages/Root', () => {
     // eslint-disable-next-line
-    const NewRoot = require('./components/Root').default;
+    const NewRoot = require('./pages/Root').default;
     render(
       <AppContainer>
         <NewRoot store={store} history={history} />
