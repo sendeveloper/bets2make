dab3327faaff8b4088316831d7decea556c6c1b4
Sendeveloper <lightmorning46@gmail.com>
2019-04-18 19:14:51 -0400

    added zoom for drawdown

--- src/components/Drawdown.js
+++ src/components/Drawdown.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Row } from 'react-bootstrap';
+import { Col, Row, Button, Modal } from 'react-bootstrap';
 import ReactHighcharts from 'react-highcharts';
 import { calcMaxMin } from '../utils/functions';
 
@@ -32,19 +32,70 @@ const BASIC_CONFIG = {
   ]
 };
 class Drawdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      zoom: false
+    };
+  }
+
+  onZoom = () => {
+    this.setState({ zoom: true });
+  };
+
+  closeAlertModal = () => {
+    this.setState({ zoom: false });
+  };
+
+  onShowZoom = () => {
+    const { zoom } = this.state;
+    const { newLineDD } = this.props;
+    const configZoom = JSON.parse(JSON.stringify(BASIC_CONFIG));
+    const { max, min } = calcMaxMin(newLineDD);
+    configZoom.series[0].data = newLineDD;
+    configZoom.yAxis.max = max;
+    configZoom.yAxis.min = min;
+    configZoom.chart.height = 300;
+    configZoom.xAxis.tickInterval = 5;
+    configZoom.yAxis.tickAmount = 5;
+
+    return (
+      <Modal show={zoom} size="lg" centered onHide={this.closeAlertModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Graph Details</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <ReactHighcharts config={configZoom} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={this.closeAlertModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  };
+
   render() {
     const { newLineDD } = this.props;
-    // let config = this.generateConfig();
-    const config = Object.assign({}, BASIC_CONFIG);
+    const config = JSON.parse(JSON.stringify(BASIC_CONFIG));
     const { max, min } = calcMaxMin(newLineDD);
     config.series[0].data = newLineDD;
     config.yAxis.max = max;
     config.yAxis.min = min;
     return (
       <Row>
-        <Col md={12}>
+        <Col md={12} style={{ position: 'relative' }}>
           <ReactHighcharts config={config} />
+          <Button
+            variant="variant"
+            onClick={this.onZoom}
+            className="zoomButton"
+          >
+            <img src="../images/zoom.png" alt="zoom" />
+          </Button>
         </Col>
+        {this.onShowZoom()}
       </Row>
     );
   }
